c16b61dd9f9f01ae03816ae8e0ab410fd55bd9da
Ryan Waters <ryananthonywaters@gmail.com>
2013-08-26 17:04:23 -0700

    initial commit and user auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surfbuddy::Application.config.secret_key_base = 'b0ee12aa9ac250685b1b66f11450ea6815a5cca4083fc5fdd46484064b20eab3fc98c194c0f9488d70d64c24a4e4cc4129d161fca8f74d287920ef1bf99430ef'
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Surfbuddy::Application.routes.draw do
+  
+  get "sessions/new"
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'  
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root to: 'users#index'
+  resources :users
+  resources :sessions
+
+end
