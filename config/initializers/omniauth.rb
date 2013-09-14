e7d620ceabab0dd6ecdf2d06ef557b764b530b62
Ryan Waters <ryananthonywaters@gmail.com>
2013-09-13 17:18:11 -0700

    m

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_OAUTH2_CLIENT_ID"], ENV["GOOGLE_OAUTH2_CLIENT_SECRET"]
+  provider :google_oauth2, '1028818941680.apps.googleusercontent.com', '79LTGhzTkQ1B_6vzkCf5JZE'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Surfbuddy::Application.routes.draw do
   get 'login', to: 'sessions#login', as: 'login'  
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  match '/auth/google_oauth2/callback' => 'sessions#create'
+  match '/auth/google_oauth2/callback' to: 'sessions#create'
 
   root to: 'sessions#login'
   
