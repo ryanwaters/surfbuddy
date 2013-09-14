620566251477196ba62dfdce4f420badc97a7572
Ryan Waters <ryananthonywaters@gmail.com>
2013-09-14 12:44:44 -0700

    production ready

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google, '1028818941680.apps.googleusercontent.com', 'a79LTGhzTkQ1B_6vzkCf5JZE'
+  provider :google, 'GOOGLE_OAUTH2_CLIENT_ID', 'GOOGLE_OAUTH2_CLIENT_SECRET'
 end
